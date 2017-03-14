aeaafa3a399d17a374a2da665943e9a9eb5ca1ff
Sam Hermans <sam.hermans@gmail.com>
2017-03-14 19:06:56 +0200

    Added titlebar and tab navigator

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 950, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 900,frame:false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
--- renderer.js
+++ renderer.js
@@ -1,8 +1,9 @@
-
+// http://paletton.com/#uid=33S110kllllD0n3s0mceFku80jC
 var net = require('net');
 var $ = require('jQuery');
 
-	
+setupWindow();
+
 var client = new net.Socket();
 client.connect(6667, 'irc.krey.net', function() {
 	$("#messagelist_console").text("Connected");
@@ -28,7 +29,38 @@ client.on('close', function() {
 	console.log('Connection closed');
 });
 
+$( window ).resize(function() {
+	doResize();
+});
+
 // FUNCTIONS
+function setupWindow() {
+	doResize();
+	newWindow("vpn-ix");
+	newWindow("irchelp");
+}
+
+var channels = {};
+
+function newWindow(tab_id) {
+	
+	// create window
+	$("<div style='display:none'><div>New window for "+ tab_id + "</div></div>").attr("id", "messagelist_" + tab_id).addClass("messagelist").appendTo($("#mainChatDiv"));
+	
+	// tab
+	var label = $('<label><label>').attr('id', "tab_" + tab_id).addClass("tab-bar__item tab-bar--material__item");
+	$("<input type='radio'>").addClass("channelbutton").data("window",tab_id).attr("name", "tab-bar-material-a").appendTo(label);
+	$('<button>').addClass("tab-bar__button tab-bar--material__button").text(tab_id).appendTo(label);
+	
+	$(label).appendTo($(tabnavbar));
+} 
+
+function doResize() {
+	var newheight = $( window ).height();
+	newheight = newheight - 150;
+	$("#mainChatDiv").height(newheight);
+}
+
 function appendTextLine(elem, text) {
 	var textline = $("<div></div>").text(text);
 	$(elem).append(textline);
@@ -36,7 +68,7 @@ function appendTextLine(elem, text) {
 }
 
 // EVENTS
-$( ".chanlink").click(function() {
+$( "input").click(function() {
 	var window = "#messagelist_" + $(this).data('window').toString();
 	$(".messagelist").hide(0, function() { $(window).show(); })
 	console.log("Showing: " + window);
